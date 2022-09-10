14bdea9fb393470c0f4b3aa28a83b358140ca4ee
Akuna Natata <nne241823@gmail.com>
2022-09-10 20:45:44 +0300

    Initial Commit

---
+++ TrafficLight 5.0/TrafficLight_5_0App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_5_0App.swift
+//  TrafficLight 5.0
+//
+//  Created by Наталья Никитина on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_5_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
